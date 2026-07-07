081a388f83f2d871825e1c796c630fdac867abd1
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-07-07 13:59:24 +0530

    if_else a dded

---
+++ lec3/if_else.js
@@ -0,0 +1,26 @@
+// que 1
+let x=-10;
+if(x>0){
+    console.log("positive");
+}
+else{
+    console.log("negative");
+}
+// que 2
+let y=17;
+if (y%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+// que 3
+let z=15;
+let a=30;
+if(z>a){
+    console.log("z is greater than a");
+}
+else{
+    console.log("a is greater than z");
+}
+
