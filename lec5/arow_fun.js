9e33616d50acab0f6d3851ff7fc997ab0c2a8dad
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-07-16 23:35:14 +0530

    arow_fun.js

---
+++ lec5/arow_fun.js
@@ -0,0 +1,18 @@
+let multiply =(a,b)=> a*b;
+console.log(multiply(5, 3));
+
+
+let iseven=(num)=> num%2===0;
+console.log(iseven(4));
+console.log(iseven(5));
+
+let greet= name => "hello "+name ;
+console.log(greet("Alice"));
+
+let max=(a,b)=> {
+    if (a>b){
+        return a;
+
+    }
+    return b;
+}
