0815e3adfe1176f5f15a90e690b62d57f69bd6de
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-07-28 19:06:55 +0530

    destruction.js

---
+++ lec6/destruction.js
@@ -0,0 +1,37 @@
+// que1:
+
+let colors = ["red", "green", "blue"];
+let [redcolour, greencolour, bluecolour] = colors;
+console.log(redcolour); // Output: red
+console.log(greencolour); // Output: green
+console.log(bluecolour); // Output: blue
+
+// que2:
+
+let car = {
+    brand: "BMW",
+    model: "M4",
+    year: 2024
+};
+let { brand, model, year } = car;
+console.log(brand); // Output: BMW
+console.log(model); // Output: M4
+console.log(year); // Output: 2024
+
+// que3:
+
+let person = {
+    name: "Anuj",
+    city: "Patna"
+};
+let { name: personName, city}=person;
+console.log(personName);
+console.log(city);
+
+// que4:
+
+let x = 100;
+let y = 200;
+[x,y]=[y,x];
+console.log(x); // Output: 200
+console.log(y); // Output: 100
