ebff96ebf4e700e33c4546ccfb434279d0ed678c
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-08-02 21:02:53 +0530

    index.html

---
+++ lec7/DOM/script.js
@@ -0,0 +1,8 @@
+// .getElementById is used to select an element by its ID
+const heading = document.getElementById("heading");
+
+console.log(heading);
+
+
+// .innerText is used to change the text of the element
+heading.innerText = "Welcome to JavaScript";
